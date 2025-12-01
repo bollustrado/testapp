692d9bf6f2d0749ef0b21de1be93a957d95974f4
bollustrado <netcat.exe@gmail.com>
2025-12-01 15:21:13 +0300

    fix

--- test.sh
+++ test.sh
@@ -4,21 +4,29 @@ set -euo pipefail
 
 BRANCH_PREFIXES=("develop_" "test_")
 MAX_AGE_DAYS=15                       # Максимальный возраст веток в днях
-REPO_PATH="/root/testapp"
+REPO_PATH="."
 
 
 cd "$REPO_PATH" || exit 1
 
 git fetch --all --prune
 
-git for-each-ref --format='%(refname:short) %(committerdate:unix)' refs/heads/ | while read -r BRANCH TS; do
-    case "$BRANCH" in
-        main|master|develop)
-            continue
-            ;;
-    esac
+for branch in $(git for-each-ref --format='%(refname:short) %(committerdate:unix)' refs/heads/); do
+  BRANCH=$(echo $branch|awk '{print $1}')
+#  echo $BRANCH
+  TS=$(echo $branch|awk '{print $2}')
+  echo $TS
+  for prefix in "${BRANCH_PREFIXES[@]}"; do
+      if [[ "$BRANCH" == "$prefix"* ]]; then
+        echo "Кастомная ветка: '$prefix'"
+        NOW=$(date +%s)
+        echo "now $NOW"
+        echo "ts $TS"
+        DIFF=$(( (NOW - TS) / 86400 ))
+        echo "diff $DIFF"
+      fi
+  done
 
-    NOW=$(date +%s)
-    DIFF=$(( (NOW - TS) / 86400 ))
-    echo $DIFF
+
+
 done
